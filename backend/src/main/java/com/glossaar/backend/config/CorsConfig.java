09cab382c3c790a7e1373c6f136efe37d98c90e9
Liisi Eenmaa <lieenm@taltech.ee>
2026-03-20 10:32:55 +0200

    #16 Add "Add Word" page and global CORS configuration
    - Implemented AddForm component with word and explanation inputs, live preview, and submit button
    - Integrated GlossarClient API call for saving words (currently for localhost url)
    - Added global CORS configuration (CorsConfig.java) to allow requests from frontend

--- backend/src/main/java/com/glossaar/backend/config/CorsConfig.java
+++ backend/src/main/java/com/glossaar/backend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.glossaar.backend.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,7 +12,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("*");
